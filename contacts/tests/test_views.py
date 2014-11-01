f26551602d1df3f607fc7992ada03888d360d362
Stefan Huynh <huynh.stefan@gmail.com>
2014-11-01 16:28:30 -0400

    Add tests for contacts post request

--- contacts/tests/test_views.py
+++ contacts/tests/test_views.py
@@ -3,12 +3,15 @@ from django.core.urlresolvers import reverse
 from contacts.models import Contact
 
 class ContactListViewTests(TestCase):
-    def test_with_no_contacts(self):
+    """
+    GET requests
+    """
+    def test_get_with_no_contacts(self):
         response = self.client.get(reverse('contacts:contact_list'))
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(response.context['contacts'], [])
     
-    def test_with_contacts(self):
+    def test_get_with_contacts(self):
         add_contact('Charlie', 'Brown', 'charlie@peanuts.com')
         add_contact('Linus', 'van Pelt', 'linus@peanuts.com')
         response = self.client.get(reverse('contacts:contact_list'))
@@ -16,15 +19,44 @@ class ContactListViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context['contacts']), 2)
         self.assertContains(response, 'Charlie Brown')
+        self.assertContains(response, 'Linus van Pelt')
+    
+    """
+    POST requests
+    """
+    def test_post_with_valid_data(self):
+        valid_data = {
+            'first_name': 'Charlie', 
+            'last_name': 'Brown', 
+            'email': 'charlie@peanuts.com'
+        }
         
-
-# class ContactCreateViewTests(TestCase):
-#     def test_with_a_get_request(self):
-#
-#     def test_with_a_post_request(self):
+        response = self.client.post(
+            reverse('contacts:contact_list'), valid_data
+        )
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(Contact.objects.all()), 1)
+    
+    def test_post_with_invalid_data(self):
+        invalid_data = {
+            'first_name': '',
+            'last_name': '',
+            'email': ''
+        }
+        
+        response = self.client.post(
+            reverse('contacts:contact_list'), invalid_data
+        )
+        
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(len(Contact.objects.all()), 0)
 
 
 def add_contact(first_name, last_name, email):
     contact = Contact.objects.get_or_create(
         first_name=first_name, last_name=last_name, email=email
     )
+    
+    
+    
