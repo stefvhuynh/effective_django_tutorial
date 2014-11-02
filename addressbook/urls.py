c5737392563b7ce0368361e794624b8e4d1f1334
Stefan Huynh <huynh.stefan@gmail.com>
2014-11-02 11:44:06 -0500

    Add support for static files and a css reset

--- addressbook/urls.py
+++ addressbook/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^contacts/', include('contacts.urls', namespace='contacts')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
--- contacts/models.py
+++ contacts/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=255, default=None)
-    last_name = models.CharField(max_length=255, default=None)
-    email = models.EmailField(default=None)
+    first_name = models.CharField(
+        max_length=255, default=None, help_text='First Name:'
+    )
+    last_name = models.CharField(
+        max_length=255, default=None, help_text='Last Name:'
+    )
+    email = models.EmailField(
+        max_length=255, default=None, help_text='Email:'
+    )
     
     def __unicode__(self):
         return ' '.join([self.first_name, self.last_name])
